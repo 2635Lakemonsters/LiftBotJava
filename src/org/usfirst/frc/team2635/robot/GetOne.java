819aace02ac323a8479f3056e40294339b5443ff
Tristan Thompson <catsrul10@gmail.com>
2015-07-27 18:50:57 -0700

    more folder stuff?

---
+++ src/org/usfirst/frc/team2635/robot/GetOne.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team2635.robot;
+
+public class GetOne implements IOutput
+{
+
+	@Override
+	public Object getOutput()
+	{
+	   return 1.0;
+	}
+
+}
---
+++ src/org/usfirst/frc/team2635/robot/IOutput.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team2635.robot;
+
+public interface IOutput 
+{
+	public Object getOutput();
+	
+	//TODO: More comments! (Infinite (Compiler ignores them))
+	
+	
+	
+
+}
